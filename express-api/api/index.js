ad916f2270ac2a3b4b786ab25e641ac5910af9e8
DezkaReid <elmaildeldezkareid@gmail.com>
2019-03-08 18:25:29 -0600

    added cors

--- express-api/api/index.js
+++ express-api/api/index.js
@@ -1,6 +1,9 @@
 const express = require('express')
+const cors = require('cors')
 const apiRouter = express.Router()
 const usersRouter = require('./users')
+
+apiRouter.use(cors())
 
 apiRouter.all('/', function (req, res) {
   res.json({ name: 'Apimon', version: 1.0 })
